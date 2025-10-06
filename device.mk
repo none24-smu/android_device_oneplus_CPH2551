423d9aa5da38a7b3ad040beff921b74b359351f0
none24smu <none24smu@users.noreply.github.com>
2025-10-05 21:45:12 -0400

    CPH2551: Fix ueventd.rc module conflict
    Remove ueventd.qcom.rc from Android.mk as it conflicts with system's
    ueventd.rc. Instead use PRODUCT_COPY_FILES to install device-specific
    ueventd rules to vendor partition.
    
    This fixes build error:
    MODULE.TARGET.ETC.ueventd.rc already defined by system/core/rootdir
    
    Change-Id: I6bc0dee65e97416c4b23580d58725e7a8bd4e9b0

--- device.mk
+++ device.mk
@@ -107,13 +107,13 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     init.target.rc \
     init.qcom.usb.rc \
-    init.qti.kernel.rc \
-    ueventd.qcom.rc
+    init.qti.kernel.rc
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
+    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
+    $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
 
 # Media
 PRODUCT_COPY_FILES += \
