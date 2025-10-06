edc2687a9717e686f73dd7d9dc875b69926b19bd
none24smu <none24smu@users.noreply.github.com>
2025-10-05 22:05:30 -0400

    CPH2551: Remove non-existent SELinux policy directories
    Remove references to sepolicy/private and sepolicy/public directories
    that don't exist. Only sepolicy/vendor directory contains our custom
    policies.
    
    This fixes the generic ckati build failure:
      build/make/core/main.mk:1117: error: Build failed
    
    The build was failing because it was trying to include non-existent
    SELinux policy directories.
    
    Change-Id: I5b8d88d5af271aee00d6a76e861f61ae70d9ec3c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -159,8 +159,6 @@ VENDOR_SECURITY_PATCH := 2024-12-01
 
 # SELinux
 include device/qcom/sepolicy_vndr/legacy-um/SEPolicy.mk
-SYSTEM_EXT_PRIVATE_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/private
-SYSTEM_EXT_PUBLIC_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/public
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Treble
