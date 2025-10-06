643aa01ea54f711c3d8cd05c92bfbe4341fae174
none24smu <none24smu@users.noreply.github.com>
2025-10-05 22:12:47 -0400

    CPH2551: Fix duplicate fstab.qcom installation
    Remove PRODUCT_COPY_FILES entry for fstab.qcom to vendor/etc since
    it's already installed by Android.mk prebuilt module. This prevents
    duplicate installation rules.
    
    Keep only the ramdisk copies needed for recovery and vendor_ramdisk.
    
    This fixes build error:
      error: overriding commands for target out/target/product/CPH2551/vendor/etc/fstab.qcom
    
    Change-Id: I9713269a140318960c35cefe6bd886ce1dad698a

--- device.mk
+++ device.mk
@@ -104,7 +104,6 @@ PRODUCT_PACKAGES += \
     init.qti.kernel.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR)/etc/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/ueventd.rc:$(TARGET_COPY_OUT_VENDOR)/etc/ueventd.rc
